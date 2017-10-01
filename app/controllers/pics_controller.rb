b7a329c973df5c27f459163bf569f12a71b4a19a
Rhonda Tinkham <rtinkham@live.com>
2017-10-01 01:00:55 +0000

    Controller actions and views added"

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -1,6 +1,11 @@
 class PicsController < ApplicationController
+  before_action :find_pic, only: [:show, :edit, :update, :destroy]
   
   def index
+    @pics = Pic.all.order("created_at DESC")
+  end
+  
+  def show
   end
   
   def new
@@ -9,12 +14,43 @@ class PicsController < ApplicationController
   
   def create
     @pic = Pic.new(pic_params)
+    
+    if @pic.save
+      flash[:success] = "Congrats! Pic was posted!"
+      redirect_to @pic
+    else
+      flash[:danger] = "Oops! Pic was not posted. Try again."
+      render 'new'
+    end
+  end
+  
+  def edit
+  end
+  
+  def update
+    if @pic.update(pic_params)
+      flash[:warning] = "Yea! Pic was updated!"
+      redirect_to @pic
+    else
+      flash[:danger] = "Yikes! Pic was not updated. Try again."
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    @pic.destroy
+    flash[:danger] = "Your pic was deleted!" 
+    redirect_to root_path
   end
   
   private
   
   def pic_params
     params.require(:pic).permit(:title, :description)
+  end
+  
+  def find_pic
+    @pic = Pic.find(params[:id])
   end
   
 end
